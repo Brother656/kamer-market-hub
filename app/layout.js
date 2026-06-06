7501eeefdc18f4b77454c742e3c995c68d3bf622
Maurice Cedric <brotherced656@gmail.com>
2026-06-06 06:44:12 +0100

    Ajout de Vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body>{children}
+        <Analytics />
+      </body>
     </html>
   )
 }
